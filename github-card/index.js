1a79eec5eccccbf1976068cb6b68290ab5f6f86a
Yiğitcan <43004864+YigitcanGuney@users.noreply.github.com>
2021-09-25 00:51:25 +0300

    hatalar güncellendi

--- github-card/index.js
+++ github-card/index.js
@@ -1,125 +1,108 @@
+
+let contentName = document.querySelector(".content-profile-name");
+let contentUserName = document.querySelector(".content-profile-username");
+let repoCount = document.querySelector(".repo-count");
+let repoSize = document.querySelector(".repo-size");
+let fieldset = document.querySelector("fieldset");
+let profileImg = document.querySelector("#profile-img")
+
+
+// username ve isim bilgisi çekildi
+const getUser = async (username) => {
+    const url = "https://api.github.com/users/";
+
+    await fetch(url + username)
+        .then(res => res.json())
+        .then(data => {
+            contentName.innerHTML = data.name;
+            contentUserName.innerHTML = `@${data.login}`;
+            profileImg.src = data.avatar_url;
+        })
+}
+
+const getRepos = async (username) => {
+    const url = "https://api.github.com/users/";
+    const repoUrl = "/repos?per_page=100";
+    let langItem = [];
+    let size = [];
+    let langSize = [];
+
+    await fetch(url + username + repoUrl)
+        .then(res => res.json())
+        .then(data => {
+            const langList = document.querySelector(".lang-ratio");
+            
+            repoCount.innerHTML = data.length;
+            data.map(item => {
+                size += item.size;
+                langSize.push(item.size);
+                langItem.push(item.language);
+            })
+            const totalItem = langItem.length * 10;
+            console.log(totalItem);
+            const uniqueLangItem = [... new Set(langItem)];
+
+            uniqueLangItem.forEach(item => {
+                
+                  
+                if(item != null) {
+                    
+                    const langCard = document.createElement("div");
+                    langCard.classList.add("lang-ratio-card");
+                    const langRatio = document.createElement("div");
+                    langRatio.classList.add("lang-ratio-content");
+                    const langSpanName = document.createElement("span");
+                    langSpanName.classList.add("lang-name");
+                    const langSpanRatio = document.createElement("span");
+                    langSpanRatio.classList.add("lang-ratio-value");
+                    const langImg = document.createElement("img");
+                    langImg.setAttribute("src","");
+                    langImg.setAttribute("alt","lang_Img");
+
+                    langSpanName.innerText = item;
+                    const langPercent = `${((item.length * 100) / totalItem).toFixed(2)}%`
+
+                    langSpanRatio.innerText = langPercent;
+
+                    langRatio.appendChild(langSpanName);
+                    langRatio.appendChild(langSpanRatio);
+                    langCard.appendChild(langImg);
+                    langCard.appendChild(langRatio);
+                    langList.appendChild(langCard);
+                    
+                    
+                }
+            })
+            repoSize.innerHTML = `${(size / 1048576).toFixed(2)} MB`;
+        })
+}
+
+const buttonClick = (e) => {
+    const searchName = document.querySelector("input[name='search-username']");
+    let getValue = searchName.value;
+
+    if (getValue.length == 0) {
+        return;
+    }
+
+    console.log(getValue);
+
+
+    return getUser(getValue), getRepos(getValue);
+}
+
+
+
 // form elemanları sayfa yenilememsi için 
 let form = document.querySelector(".form-control");
 
-let size = 0;
-let sizeoflang = 0;
+// let size = 0;
+// let sizeoflang = 0;
 form.addEventListener("click", (e) => {
     e.preventDefault();
 })
 
 
-// profile bilgileri veri çekilmesi
-const getUser = (e) => {
-    size = 0;
-    let langKey = [];
-    let langValue = [];
-    let uniqueKey = [];
-    let searchname = document.querySelector("input[name='search-username']");
-    let username = searchname.value;
-
-    let url = `https://api.github.com/users/${username}`
-    let repoUrl = `https://api.github.com/users/${username}/repos`
-
-    let contentName = document.querySelector(".content-profile-name");
-    let contentUserName = document.querySelector(".content-profile-username");
-    let repoCount = document.querySelector(".repo-count");
-    let repoSize = document.querySelector(".repo-size");
-    let fieldset = document.querySelector("fieldset");
-
-
-
-
-
-    let profileImg = document.querySelector("#profile-img");
-
-    // profil resim ve isimleri fetch fonksiyonu
-    async function fetchProfile() {
-        const response = await fetch(url);
-        const profile = await response.json();
-        return profile;
-    }
-
-    // profil resim ve isimleri dom element eklenmesi
-    fetchProfile().then(data => {
-        contentName.innerHTML = data.name;
-        profileImg.src = data.avatar_url;
-        contentUserName.innerHTML = `@${data.login}`
-    })
-
-    // repo bilgilerinin alındığın fetch fonskiyonu
-    async function fetchReposCount() {
-        const response = await fetch(repoUrl);
-        const repos = await response.json();
-        return repos;
-    }
-
-    // repo bilgilerinin doma eklenmesi
-    fetchReposCount().then(data => {
-        repoCount.innerHTML = data.length;
-
-
-
-        data.map(item => {
-
-            // repolarda kullanılan yazılım dillerinin çekilmesi 
-            fetch(item.languages_url).then(res => res.json())
-                .then(data => {
-                    if (!(Object.keys(data).length === 0)) {
-                        langKey.push(...Object.keys(data));
-                        langValue.push(...Object.values(data));
-                    }
-                    langValue.forEach((item, index) => {
-                        size += item
-                    })
-
-                    uniqueKey = [...new Set(langKey)];
-
-                    
-                  // yazılım dilleri ve boyunlarının toplam repoya oranlanıp doma eklenmesi 
-                    for (let i = 0; i < uniqueKey.length; i++) {
-                        for (let x =0;x < langValue.length; x++) {
-
-                            attSrc = "src";
-                            attAlt = "alt"
-                            let langDiv = document.createElement("div");
-                            let langDivCard = document.createElement("div")
-                            let langDivContent = document.createElement("div")
-                            let spanLangName = document.createElement("span")
-                            let spanLangRatio = document.createElement("span")
-                            let langImg = document.createElement("img")
-                            langImg.setAttribute(attSrc,"")
-                            langImg.setAttribute(attAlt,"lang-img")
-                            langDiv.classList.add("lang-ratio")
-                            langDivCard.classList.add("lang-ratio-card")
-                            langDivContent.classList.add("lang-ratio-content")
-                            spanLangRatio.classList.add("lang-ratio-value")
-
-                            spanLangName.innerHTML = uniqueKey[i];
-                            spanLangRatio.innerText =`${((langValue[x]/size)*100).toFixed(2)}%`
-                            langDivContent.appendChild(spanLangName);
-                            langDivContent.appendChild(spanLangRatio);
-                            langDivCard.appendChild(langImg);
-                            langDivCard.appendChild(langDivContent);
-                            langDiv.appendChild(langDivCard);
-                            fieldset.appendChild(langDiv);
-                        }
-
-                    }
-                    repoSize.innerHTML = `${(size / 1048576).toFixed(2)} MB`;
-                });
-
-        });
-
-
-    })
-
-
-
-
-
-
-    fieldset.style.display = "block";
-}
 
 
 
